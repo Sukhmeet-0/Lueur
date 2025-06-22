415c11a01b7be5b4d4aefef16b6dd168beb0e8e8
Sukhmeet-0 <xsukhmeet@gmail.com>
2025-06-22 19:47:36 +0530

    adding search sort functionality in colletion page and now need to set product display page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
-  return (
+  const {productId} = useParams();
+  const {products} = useContext(ShopContext);
+  const [productData,setProductData] = useState(false);
+  const [image,setImage] = useState('');
+
+  const fetchProductData = async()=>{
+    products.map((item,index)=>{
+      if(item._id === productId){
+        setProductData(item);
+        setImage(item.image[0]);
+        return null;
+      }
+    })
+  }
+
+  useEffect(()=>{
+    fetchProductData();
+  },[productId])
+  return productData? (
     <div>
       
     </div>
-  )
+  ):<div className='opacity-0'></div>
 }
 
 export default Product
